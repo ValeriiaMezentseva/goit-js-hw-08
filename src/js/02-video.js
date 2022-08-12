f3b4d01a4fd17b7c1c7c401212b06f24d45ea633
ValeriiaM23 <mezencevavalerie@gmail.com>
2022-08-12 17:33:03 +0200

    adding video player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,19 +1,37 @@
-// var throttle = require('lodash.throttle')
-// import player from '@vimeo/player'
-// const iframe = document.querySelector('iframe');
-// const player = new Vimeo.Player(iframe);
 
-// let savedTime;
+import throttle from 'lodash.throttle';
+import Player from "@vimeo/player";
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
-// function onPlay(data) {
-//     localStorage.setItem("videoplayer-current-time", JSON.stringify(data));
-// };
 
-// player.on('timeUpdate', throttle(onPlay, 1000));
+let parsedTime;
 
-//     savedTime = localStorage.getItem("videoplayer-current-time");
-// if (savedTime) {
-//     parsedTime = (JSON.parse(savedTime));
-// } else {
-//     parsedTime = 0;
-// };
+function onPlay(data) {
+    const videoTime = JSON.stringify(data);
+    localStorage.setItem("videoplayer-current-time", videoTime);
+};
+player.on('timeUpdate', throttle(onPlay, 1000));
+iframe.addEventListener('play', onPlay);
+
+ let savedTime = localStorage.getItem("videoplayer-current-time");
+
+
+function checkParseTime(savedTime) {
+    if (savedTime) {
+    parsedTime = JSON.parse(savedTime);
+} else {
+    parsedTime = 0;
+};   
+};
+checkParseTime(savedTime);
+
+player.setCurrentTime(parsedTime.seconds).then(function () { }).catch(function (error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+        default:
+            break;
+    }
+});
+
